a429b0c1fcbb4a8a86d0f860dd5c6f15aa08de41
Puneet Kashyap <pkashyaps@gmail.com>
2017-12-11 16:46:26 -0500

    initial draft of db store

--- build.sbt
+++ build.sbt
@@ -1,11 +1,17 @@
 name := """akka-db"""
-
-version := "1.0"
+organization := "com.akkademy-db"
+version := "0.0.1-SNAPSHOT"
 
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+  "com.typesafe.akka" %% "akka-remote" % "2.3.6",
   "com.typesafe.akka" %% "akka-actor" % "2.3.11",
   "com.typesafe.akka" %% "akka-testkit" % "2.3.6" % "test"
+
 )
+
+mappings in (Compile, packageBin) ~= { _.filterNot { case (_, name) =>
+  Seq("application.conf").contains(name)
+}}
--- src/main/scala/com/akka/AkkaDb.scala
+++ src/main/scala/com/akka/AkkaDb.scala
@@ -1,9 +1,10 @@
 package com.akka
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorSystem, Props, Status}
 import akka.event.Logging
+
 import scala.collection.mutable
-import com.akka.messages.SetRequest
+import com.akka.messages.{GetRequest, KeyNotFoundException, SetRequest}
 
 class AkkaDb extends Actor{
   val map = new mutable.HashMap[String, Object]()
@@ -12,7 +13,22 @@ class AkkaDb extends Actor{
     case SetRequest(key, value) => {
       log.info("received SetRequest - key: {} value: {}", key,value)
       map.put(key,value)
+      sender() ! Status.Success
     }
-    case o => log.info("received unknown message: {}", o)
+    case GetRequest(key) => {
+      log.info("received GetRequest - key: {}", key)
+      val response: Option[Object] = map.get(key)
+      response match {
+        case Some(x) => sender() ! x
+        case None => sender() ! Status.Failure(new KeyNotFoundException(key))
+      }
+
+    }
+    case o => Status.Failure(new ClassNotFoundException())
   }
+}
+
+object Main extends App {
+  val system = ActorSystem("akkaDB")
+  val helloActor = system.actorOf(Props[AkkaDb], name = "akka-db")
 }
